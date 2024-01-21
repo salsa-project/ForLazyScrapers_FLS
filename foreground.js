d60cbbf7b84f4ff143f748e84bb97cf2b611a26e
salsa-project <intenso524@gmail.com>
2024-01-21 07:49:22 +0100

    create token generator

--- foreground.js
+++ foreground.js
@@ -5,3 +5,24 @@
 // and injected into the same or different pages.
 
 console.log("This prints to the console of the page (injected only if the page url matched)")
+
+
+/*================================
+        TOKEN GENERATOR
+================================*/
+function generateRandomToken(length) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let token = '';
+  
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      token += characters.charAt(randomIndex);
+    }
+  
+    return token;
+  }
+  
+  // Example usage:
+  const randomToken = generateRandomToken(16);
+  console.log(randomToken);
+  
